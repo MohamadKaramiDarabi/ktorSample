e8d5196e8ab3e11ed9fd86792c6e819a775efb91
karami <mohamad.karami.darabi@gmail.com>
2022-01-28 18:20:22 +0330

    some change

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -5,7 +5,7 @@ import io.ktor.server.netty.*
 import com.example.plugins.*
 
 fun main() {
-    embeddedServer(Netty, port = 8080, host = "188.121.122.106") {
+    embeddedServer(Netty, port = 3000, host = "188.121.122.106") {
         configureRouting()
     }.start(wait = true)
 }
